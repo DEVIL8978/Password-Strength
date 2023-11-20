e117d1a08ba9b809227dec2453fb98cec7f7d6bf
Singh <122664158+DEVIL8978@users.noreply.github.com>
2023-11-20 15:28:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+ 
+const password = document.getElementById('password');
+const background = document.getElementById('background');
+
+
+password.addEventListener('input', (e) => {
+  const input = e.target.value;
+  const length = input.length;
+  const blurValue = 20 - length * 1;
+  if(length ==0){
+  	alert('empty!');
+  }
+  background.style.filter = `blur(${blurValue}px)`;
+});
+
